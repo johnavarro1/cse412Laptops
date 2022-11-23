8a4bc823ae1c12eb0d2a34a2beb2949e28bd1133
mlnavar3 <mlnavar3@asu.edu>
2022-11-23 13:20:09 -0700

    search page and results page updated

--- src/main/java/com/example/cse412laptops/SearchPageController.java
+++ src/main/java/com/example/cse412laptops/SearchPageController.java
@@ -2,28 +2,61 @@ package com.example.cse412laptops;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 
+import java.io.IOException;
+
 public class SearchPageController {
+
+    @FXML
+    private CheckBox Lenovo;
+
+    @FXML
+    private CheckBox apple;
+
+    @FXML
+    private CheckBox chromeOS;
+
+    @FXML
+    private CheckBox dell;
+
+    @FXML
+    private CheckBox linux;
+
+    @FXML
+    private CheckBox macOS;
+
+    @FXML
+    private CheckBox microsoft;
+
+    @FXML
+    private CheckBox otherManufacturer;
+
+    @FXML
+    private CheckBox otherOS;
+
+    @FXML
+    private RadioButton over2000;
+
+    @FXML
+    private RadioButton range1000_1500;
+
+    @FXML
+    private RadioButton range1500_2000;
+
+    @FXML
+    private RadioButton range500_1000;
+
     @FXML
     private RadioButton under500;
 
     @FXML
-    private Label selectedLabel;
+    private CheckBox windows;
 
     @FXML
-    public void searchBtnClick(ActionEvent actionEvent) {
-
-        // test if radio button is selected
-        if(under500.isSelected())
-        {
-            selectedLabel.setText("Display laptops under $500");
-        }
-        else
-        {
-            selectedLabel.setText("No selection");
-        }
+    void searchBtnClick(ActionEvent event) {
 
     }
+
 }
--- src/main/java/com/example/cse412laptops/resultsController.java
+++ src/main/java/com/example/cse412laptops/resultsController.java
@@ -1,4 +1,22 @@
 package com.example.cse412laptops;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+
 public class resultsController {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private TableView<?> resultsTable;
+
+    @FXML
+    void backBtn_click(ActionEvent event){
+
+    }
+
 }
